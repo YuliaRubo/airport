860eea04d90041ef42dc3d464ed0cac16a291eff
Ulya Ivancova <ulya-rubo@mail.ru>
2021-06-10 09:38:53 +0200

    commit3

--- src/util/flightUtil.java
+++ src/util/flightUtil.java
@@ -16,26 +16,36 @@ public class flightUtil {
         int id = Integer.parseInt(flightArr[0]);
         int plane_id = Integer.parseInt(flightArr[1]);
         int pilot_id = Integer.parseInt(flightArr[2]);
-        Date data = StrToDate(flightArr[3]);
+       Date data = ??????(flightArr[3]);
 //         time = StrToTime(flightArr[4]);
 
         String time = flightArr[4];
         String number_flight = flightArr[5];
         return new Flight(id, plane_id, pilot_id, data, time, number_flight);
     }
-private static Date StrToDate(String stringDate){
-    DateFormat date = new SimpleDateFormat("dd/MM/yyyy");
-    Date nDate1 = new Date();
-    try{
-        Date nDate2 = date.parse(stringDate);
-        date.format(nDate2);
-    }
-     catch(Exception e){
-        System.out.println(e);
-    }
-
-    return nDate1;
+private  Date StrToDate(String stringDate){
+   SimpleDateFormat sim = new SimpleDateFormat("yyyy.MM.dd");
+   Date d = new Date();
+   try{
+       d = sim.parse(stringDate);
+   } catch (ParseException e) {
+       e.printStackTrace();
+   }
+    return d;
 }
+//    return (sim.format(d));
+//    DateFormat date = new SimpleDateFormat("yyyy.MM.dd");
+//    Date nDate1 = new Date();
+//    try{
+//        Date nDate2 = date.parse(stringDate);
+//        date.format(nDate2);
+//    }
+//     catch(Exception e){
+//        System.out.println(e);
+//    }
+
+//    return d;
+//}
 //    private static Date StrToDate(String s) {
 //        SimpleDateFormat format = new SimpleDateFormat();
 //        format.applyPattern("yyyy.MM.dd");
